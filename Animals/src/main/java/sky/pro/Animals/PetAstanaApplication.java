192fede60eea482f216a2ca0bc5b800e06bf8bc1
Lorgar-Avrelian <victor-14-244@mail.ru>
2023-11-08 23:01:47 +0300

    Project initialization

---
+++ Animals/src/main/java/sky/pro/Animals/PetAstanaApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.Animals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAstanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAstanaApplication.class, args);
+	}
+
+}
---
+++ Animals/src/test/java/sky/pro/Animals/PetAstanaApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.Animals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAstanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
