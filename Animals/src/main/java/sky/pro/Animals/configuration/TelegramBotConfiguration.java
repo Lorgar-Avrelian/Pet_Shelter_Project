9fee9bd3e07d85925197b0ff6434f421a670ca89
Alexandrov <alex.44747.alex@gmail.com>
2023-11-13 17:40:26 +0300

    'rcgthtvtyn'

--- Animals/src/main/java/sky/pro/Animals/configuration/TelegramBotConfiguration.java
+++ Animals/src/main/java/sky/pro/Animals/configuration/TelegramBotConfiguration.java
@@ -10,10 +10,8 @@ import org.springframework.context.annotation.Configuration;
 public class TelegramBotConfiguration {
     @Value("${telegram.bot.token}")
     private String token;
-
     @Bean
     public TelegramBot telegramBot() {
-
         TelegramBot bot = new TelegramBot(token);
         bot.execute(new DeleteMyCommands());
         return bot;
