bd37936a13f923fdbd7a4e206c56fe35106c0cd7
Lorgar-Avrelian <victor-14-244@mail.ru>
2023-11-14 17:08:56 +0300

    Подключил Liquibase, подключил БД, создал в БД три таблицы: для хранения данных о клиентах (client), сотрудниках (employee) и питомцах (pet)

--- Animals/src/main/java/sky/pro/Animals/PetAstanaApplication.java
+++ Animals/src/main/java/sky/pro/Animals/PetAstanaApplication.java
@@ -13,7 +13,4 @@ public class PetAstanaApplication {
     public static void main(String[] args) {
         SpringApplication.run(PetAstanaApplication.class, args);
     }
-	public static void main(String[] args) {
-		SpringApplication.run(PetAstanaApplication.class, args);
-	}
 }
