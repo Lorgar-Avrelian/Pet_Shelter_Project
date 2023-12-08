b78cd21bf8e9a2c0958021028450a6d51513795d
Alexandrov <alex.44747.alex@gmail.com>
2023-12-08 12:50:13 +0300

    всё удалил

--- Animals/src/main/java/sky/pro/Animals/PetAstanaApplication.java
+++ Animals/src/main/java/sky/pro/Animals/PetAstanaApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@OpenAPIDefinition
-@EnableScheduling
 public class PetAstanaApplication {
 
 	public static void main(String[] args) {
--- Animals/src/main/java/sky/pro/Animals/repository/InfoRepository.java
+++
@@ -1,9 +0,0 @@
-package sky.pro.Animals.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import sky.pro.Animals.entity.Info;
-
-@Repository
-public interface InfoRepository extends JpaRepository<Info, Long> {
-}
